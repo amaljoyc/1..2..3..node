64308334d65cbd86f7d9d7cc8fe38c7768904762
amaljoyc <amaljoyc@gmail.com>
2016-09-25 21:45:43 +0200

    support post req

--- express/api/routes/router.js
+++ express/api/routes/router.js
@@ -11,4 +11,13 @@ router.get('/hello', function(request, respose) {
 	respose.send('Hello there!');
 });
 
+router.get('/hello-json', function(request, respose) {
+	respose.json({ message: 'Hello there!' });
+});
+
+router.post('/push', function(request, respose) {
+	var data = request.body;
+	respose.send('Received push request with json: ' + JSON.stringify(data));
+});
+
 module.exports = router;
