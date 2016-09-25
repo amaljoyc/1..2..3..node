17e427a8f814944ca511575e5742950ff2b887f9
amaljoyc <amaljoyc@gmail.com>
2016-09-25 22:55:56 +0200

    use swagger

--- express/api/server.js
+++ express/api/server.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var SwaggerExpress = require('swagger-express-mw');
 
 var app = express();
+
+var config = {
+  appRoot: __dirname, // swagger default config
+};
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/api', require('./routes/router'));
+app.use('/api', require('./api/controllers/router'));
 
-app.listen(4321);
-console.log('Started api server at port 4321...');
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+  if (err) { throw err; }
+
+  swaggerExpress.register(app);
+  app.listen(4321);
+  console.log('Started api server at port 4321...');
+});
